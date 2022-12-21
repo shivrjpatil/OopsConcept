3e60f8fc88c607d7a5cd47c5a2be91e6f032a056
shivrjpatil <shivrajp972@gmail.com>
2022-12-21 10:53:02 +0530

    single inheritance implemetation

---
+++ Oops/Inheritance/SingleInheritance.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oops.Inheritance
+{
+    public class SingleInheritance
+    {
+       public  class car
+        {
+            public string Model;
+            public string color;
+            public int price;
+            
+            public void PrintCarInformation()
+            {
+                Console.WriteLine("model:" +Model);
+                Console.WriteLine("Color:" +color);
+                Console.WriteLine("Price:" +price);
+            }
+        }
+
+       public class Maruthi : car
+        {
+            public int milage;
+            public void PrintMilage()
+            {
+                Console.WriteLine("Milage:" +milage);
+            }
+        }
+
+       
+        
+    }
+}
---
+++ Oops/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Oops.Inheritance.SingleInheritance;
+
+namespace Oops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Maruthi maruthi = new Maruthi();
+            maruthi.Model = "alto";
+            maruthi.color = "white";
+            maruthi.price = 200000;
+            maruthi.milage = 22;
+            //maruthi.PrintCarInformation();
+            //maruthi.PrintMilage();
+            Console.ReadLine();
+        }
+    }
+}
